dc22837a1d5c1d4ad693fedb5ece82cd42f3c9fc
gotoinc <root@gotoinc.co>
2023-04-30 17:26:30 +0300

    setup/remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
 import 'semantic-ui-css/semantic.min.css'
 
 import App from './App'
 
 import './firebase'
-import { store } from 'redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<App />
 	</React.StrictMode>,
 )
