2f5a040cad213caf9ef6f8251e7a8b2f3269df95
gotoinc <root@gotoinc.co>
2023-06-07 20:10:30 +0300

    style/NotFoundPage

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,5 +1,16 @@
+import { Header } from 'semantic-ui-react'
+
 function NotFoundPage(): JSX.Element {
-	return <div>NotFoundPage</div>
+	return (
+		<div className='w-100 h-100 d-flex align-items-center justify-content-center p-4'>
+			<Header
+				textAlign='center'
+				as='h1'
+			>
+				Такої сторінки не існує
+			</Header>
+		</div>
+	)
 }
 
 export default NotFoundPage
